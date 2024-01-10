bcf4934d5796c8fd73ae951f56135c597e8a5df0
deivid94 <md031194@gmail.com>
2024-01-09 23:17:19 -0300

    feat: config experience section

---
+++ src/app/components/experience/experience.tsx
@@ -0,0 +1,14 @@
+import { SectionTitle } from "../section-title/section-title"
+import './experience.scss'
+
+export function Experience(){
+    return (
+        <div className="experience">
+        <SectionTitle text="Experience"/>
+        <p>2 years working as a WebDeveloper, and 1 year working as a Networking operations center in companys such as MAIS INTERNET</p>
+        <div className="experiencetime">
+        
+        </div>
+      </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 import Image from 'next/image'
 import { Header } from './components/header/header'
+import { Experience } from './components/experience/experience'
 import "./styles/home.scss"
 
 export default function Home() {
   return (
     <main className='container'>
      <Header/>
-     <div className="experience">
-      <h3>Experience</h3>
-      <p>2 years working as a WebDeveloper, and 1 year working as a Networking operations center in companys such as MAIS INTERNET</p>
-      <div className="experiencetime">
-
-      </div>
+      <Experience/>
       <div className="info">
         <h3>Formacao Academica</h3>
         <div className="educational-info">
@@ -28,7 +24,7 @@ export default function Home() {
         </div>
         <button>Fale Comigo</button>
       </div>
-     </div>
+     
    
      
     </main>
